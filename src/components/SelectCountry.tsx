2240c4108a90baa8ea4007e7ee9fa9746a1ae3f7
sakiynam <mamatisayevasarvinoz@gmail.com>
2024-04-20 22:35:34 +0500

    commit

--- src/components/SelectCountry.tsx
+++ src/components/SelectCountry.tsx
@@ -1,41 +1,45 @@
-import { Autocomplete, Grid, Skeleton, TextField } from "@mui/material";
+import { Autocomplete, CircularProgress, Grid, TextField } from "@mui/material";
 import useAxios from "../hooks/useAxios";
 
-const SelectCountry = (props) => {
-    const {value, setValue, label} = props;
-    const [data, loaded, error] = useAxios<any>("https://restcountries.com/v3.1/all");
-    
+interface SelectCountryProps {
+    value: any;
+    setValue: (value: any) => void;
+    label: string;
+}
+
+const SelectCountry = ({ value, setValue, label }: SelectCountryProps) => {
+    const [data, loaded, error] = useAxios<any[]>("https://restcountries.com/v3.1/all");
+
     if (loaded) {
         return (
             <Grid>
-                <Skeleton variant="rounded"  height={60}/>
+                <CircularProgress />
             </Grid>
-        )
+        );
     }
 
     if (error) {
-        return "Something went wrong"
+        return "Something went wrong";
     }
 
-    const dataFilter = data.filter(item => "currencies" in item)
+    const dataFilter = data.filter(item => "currencies" in item);
     const dataCountries = dataFilter.map(item => {
-        return `${item.flag} ${Object.keys(item.currencies)[0]} - ${item.name.common}`
-    })
-    console.log( 11, dataFilter);
-    
-    
-  return (
-    <Grid item xs={12} md={3}>
-      <Autocomplete 
-        value={value}
-        onChange={(event, newValue) => {
-            setValue(newValue)
-        }}
-        options={dataCountries} 
-        renderInput={(params) => <TextField {...params} label={label} />}
-        />
-    </Grid>
-  );
+        return `${item.flag} ${Object.keys(item.currencies)[0]} - ${item.name.common}`;
+    });
+    console.log(11, dataFilter);
+
+    return (
+        <Grid item xs={12} md={3}>
+            <Autocomplete
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                options={dataCountries}
+                renderInput={(params) => <TextField {...params} label={label} />}
+            />
+        </Grid>
+    );
 };
 
 export default SelectCountry;
